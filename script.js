5fc1849a83f8d15223cc971a34478d913e44ab14
Zahra <hizahrazadeh@gmail.com>
2021-12-30 14:11:06 -0500

    add js script for slider buttons

--- script.js
+++ script.js
@@ -10,13 +10,45 @@ menuButton.addEventListener("click", () => {
 });
 
 const sliderbtn_one = document.querySelector(".btn_one");
-const sliderbtn_two = document.querySelector(".btn_two");
+const sliderbtn_two = document.querySelector("a.btn_two");
 const sliderbtn_three = document.querySelector(".btn_three");
-const quote_one = document.querySelector(".quote");
-const author_one = document.querySelector(".author");
-const quote_two = document.querySelector(".quote.two");
+const quote_one = document.querySelector("p.quote");
+const author_one = document.querySelector("p.author");
+const quote_two = document.querySelector("p.quote.two");
 const author_two = document.querySelector(".author.two");
-const quote_three = document.querySelector(".quote.three");
+const quote_three = document.querySelector("p.quote.three");
 const author_three = document.querySelector(".author.three");
 
-sliderbtn_one.addEventListener("click", () => {});
+sliderbtn_one.addEventListener("click", () => {
+  console.log("we here");
+  quote_one.style.color = "red";
+  // quote_two.style.opacity = "1";
+  quote_two.classList.toggle("displayNone");
+  author_two.classList.toggle("displayNone");
+  quote_three.classList.toggle("displayNone");
+  author_three.classList.toggle("displayNone");
+});
+
+sliderbtn_two.addEventListener("click", () => {
+  console.log("hereee");
+  quote_two.style.display = "block";
+  author_two.style.display = "block";
+  quote_one.classList.toggle("displayNone");
+  author_one.classList.toggle("displayNone");
+  quote_three.classList.toggle("displayNone");
+  author_three.classList.toggle("displayNone");
+});
+
+sliderbtn_three.addEventListener("click", () => {
+  quote_three.style.display = "block";
+  // quote_two.style.display = "none";
+  quote_one.classList.toggle("displayNone");
+  author_one.classList.toggle("displayNone");
+  quote_two.classList.toggle("displayNone");
+  author_two.classList.toggle("displayNone");
+});
+
+// document.querySelector("a.btn_two").onclick = function () {
+//   console.log("here");
+//   quote_one.style.color = "blue";
+// };
